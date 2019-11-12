f0958d0314d51ae6ef7ec23126ce68bc8b4e3cec
Farhad <gfarhad93@gmail.com>
2019-11-12 15:10:27 +0300

    New Version for carthage

--- TLPhotoPicker/Classes/TLBundle.swift
+++ TLPhotoPicker/Classes/TLBundle.swift
@@ -12,7 +12,7 @@ open class TLBundle {
     open class func podBundleImage(named: String) -> UIImage? {
         let podBundle = Bundle(for: TLBundle.self)
         if let url = podBundle.url(forResource: "TLPhotoPickerController", withExtension: "bundle") {
-            let bundle = Bundle(url: url)
+            let bundle = Bundle(url: url) 
             return UIImage(named: named, in: bundle, compatibleWith: nil)!
         }
         return nil
