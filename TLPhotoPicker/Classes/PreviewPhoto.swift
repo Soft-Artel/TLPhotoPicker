5230dd3f9db4ea1c7d50011d20d75969fbb53f99
Murat <89196242960@mail.ru>
2020-04-09 20:36:54 +0300

    version 2.0.18

--- TLPhotoPicker/Classes/PreviewPhoto.swift
+++ TLPhotoPicker/Classes/PreviewPhoto.swift
@@ -99,10 +99,12 @@ class PreviewPhoto: UIViewController{
     @objc func editPhoto(){
         guard let delegate = TLPhotosPickerViewController.delegateEditor else { return }
         delegate.openPhotoEditor(with: image, parentVC: self, complition: self.complition)
+        self.dismiss(animated: true, completion: nil)
     }
     
     @objc func takePhoto(){
         self.complition(false)
+        self.dismiss(animated: true, completion: nil)
     }
     
     @objc func retake(){
